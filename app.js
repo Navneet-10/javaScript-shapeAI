ee411d9a1f9cb1f824f89a0fb5141bbfdc8bda6e
Navneet <navneetanand256@gmail.com>
2021-09-02 19:31:28 +0530

    console addded

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('Hey this is my first piece of JS code!!! YAY')
+console.log(1254645456)
+
+console.log({name:'Prags'})
+console.log(true)
+console.log(false)
+console.log(false,2435, 'string', {name: 'navneet'})
+console.warn("this is a warning")
+console.error("this is an error")
